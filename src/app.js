be9342e2e38d0fcdfe5afe83ad990b5260aa96e2
Damon <damondias@gmail.com>
2023-09-11 11:35:27 -0400

    feat: basic project structure

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express, { json } from "express"
+import cors  from "cors"
+import dotenv from "dotenv"
+import router from "./routes/index.routes.js";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`
+    Running API Driviagens on port ${PORT},
+    Url: http://localhost:${PORT}
+`));
